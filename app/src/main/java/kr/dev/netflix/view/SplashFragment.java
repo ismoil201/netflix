fe98d2d4b770e9ec9edc4377f21482b3ba629e22
Ismoil Jurakhonov <ismoiljoraxonov1@gmail.com>
2023-09-29 14:47:21 +0900

    created basic libllary
    navigation
    view binding

---
+++ app/src/main/java/kr/dev/netflix/view/SplashFragment.java
@@ -0,0 +1,39 @@
+package kr.dev.netflix.view;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import kr.dev.netflix.R;
+import kr.dev.netflix.databinding.FragmentSplashBinding;
+
+
+public class SplashFragment extends Fragment {
+
+    private FragmentSplashBinding binding;
+
+    public SplashFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentSplashBinding.inflate(LayoutInflater.from(container.getContext()),container,false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
