0472042a2cee8ffa966b4631681754956261bc8c
Ismoil Jurakhonov <ismoiljoraxonov1@gmail.com>
2023-09-29 17:47:44 +0900

    i'm create  login xml
    login xml ui chizdim

--- app/src/main/java/kr/dev/netflix/view/LoginFragment.java
+++ app/src/main/java/kr/dev/netflix/view/LoginFragment.java
@@ -10,11 +10,6 @@ import android.view.ViewGroup;
 
 import kr.dev.netflix.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LoginFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class LoginFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
