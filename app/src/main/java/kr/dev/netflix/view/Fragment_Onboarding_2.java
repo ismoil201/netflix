cac96187f7b80c62ba987c40b185aa9152470937
Hakimoov <hakimovhasanboy95@gmail.com>
2023-09-30 16:47:11 +0500

    as
    as

--- app/src/main/java/kr/dev/netflix/view/Fragment_Onboarding_2.java
+++ app/src/main/java/kr/dev/netflix/view/Fragment_Onboarding_2.java
@@ -60,6 +60,6 @@ public class Fragment_Onboarding_2 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment__onboarding_1, container, false);
+        return inflater.inflate(R.layout.fragment__onboarding_2, container, false);
     }
 }
