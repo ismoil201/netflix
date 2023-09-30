f047c47c9a36c098662431a0b1ab3b71047db47c
Ismoil Jurakhonov <ismoiljoraxonov1@gmail.com>
2023-09-30 21:15:39 +0900

    reapted

--- app/src/main/java/kr/dev/netflix/MainActivity.java
+++ app/src/main/java/kr/dev/netflix/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import kr.dev.netflix.databinding.ActivityMainBinding;
-import kr.dev.netflix.view.Fragment_Onboarding_1;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,11 +16,6 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.container_onboarding_1
-                , new Fragment_Onboarding_1())
-                .commit();
 
 
     }
--- app/src/main/java/kr/dev/netflix/view/Fragment_Onboarding_3.java
+++ app/src/main/java/kr/dev/netflix/view/Fragment_Onboarding_3.java
@@ -60,6 +60,6 @@ public class Fragment_Onboarding_3 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment__onboarding_1, container, false);
+        return inflater.inflate(R.layout.fragment__onboarding_3, container, false);
     }
 }
